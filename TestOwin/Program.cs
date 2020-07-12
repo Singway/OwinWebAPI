40592503454319199a346b9c6fd5c2068aeebd3e
成威 骆 <q490013473@hotmail.com>
2020-07-13 00:48:54 +0800

    初始代码

---
+++ TestOwin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOwin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>(url: baseAddress))
+            {
+                HttpClient client = new HttpClient();
+                Console.WriteLine(baseAddress);
+                Console.ReadLine();
+            }
+        }
+    }
+}
